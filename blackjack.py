5ec16dd73f77d737fc9e0db5f84be9588984ffe0
Claudia Hill <hillc255@yahoo.com>
2020-01-13 05:46:28 -0500

    initial commit

---
+++ blackjack.py
@@ -0,0 +1,33 @@
+import random
+
+'''
+Title:        BlackJack Game
+# Date:       1/13/2020
+Author:       Claudia Hill
+Purpose:      Create BlackJack Game with Python
+            Milestone 2 - Udemy Python Bootcamp
+Description:  Original code in PyCharm IDE
+'''
+
+class BlackJack():
+
+    print('Welcome to BlackJack!')
+
+    #modules def
+    def cards_played(self):
+        pass
+
+
+    def dealer(self):
+        pass
+
+
+
+    def player(self):
+        pass
+
+
+
+if __name__ == "__main__":
+
+    my_blackJack = BlackJack()
